a980c5963ff5a442a7dade3cccc922045aff0c79
Murilo Stein <murilo.r.stein@gmail.com>
2025-01-18 11:31:48 -0300

    feat: changing @ignite-ui to @stein-ingnite-ui

--- packages/docs/src/components/ColorsGrid.tsx
+++ packages/docs/src/components/ColorsGrid.tsx
@@ -1,4 +1,4 @@
-import { colors } from '@ignite-ui/tokens'
+import { colors } from '@stein-ignite-ui/tokens'
 import { getContrast } from 'polished'
 
 export function ColorsGrid() {
---
+++ packages/react/CHANGELOG.md
@@ -0,0 +1,7 @@
+# @stein-ignite-ui/react
+
+## 1.1.0
+
+### Minor Changes
+
+- Add display name to all components
